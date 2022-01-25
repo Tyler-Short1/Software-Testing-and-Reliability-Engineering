b3b4d336f423fcf4650912389bb203133a22ce6b
Tyler-Short1 <Tyler.A.Short1@Outlook.com>
2022-01-25 11:44:32 +0000

    Added accelerate, brake and refuel. Capped fuel and max speed limit.

---
+++ Car.py
@@ -0,0 +1,29 @@
+class Car:
+    def __init__(self, currentspeed, maxspeed, fuellevel):
+        self.currentspeed = currentspeed
+        self.maxspeed = maxspeed
+        self.fuellevel = fuellevel
+
+    def accelerate(self, acceleration):
+        if self.fuellevel == 0:
+            self.currentspeed = 0
+            self.acceleration = 0
+
+        elif self.currentspeed < self.maxspeed:
+            self.currentspeed += acceleration
+            self.fuellevel = self.fuellevel-1
+            if self.currentspeed > self.maxspeed:
+                self.currentspeed = self.maxspeed
+
+    def brake(self, brakespeed):
+
+        if self.currentspeed > 0:
+            self.currentspeed -= brakespeed
+            if self.currentspeed < 0:
+                self.currentspeed = 0
+
+    def refuel(self, refuel):
+        if self.fuellevel < 100:
+            self.fuellevel += refuel
+            if self.fuellevel > 100:
+                self.fuellevel = 100
